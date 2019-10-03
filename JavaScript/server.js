39c684e559f0f1a8e574316f7527ff1f534fa70d
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2019-10-03 17:25:53 +0300

    Improve server

--- JavaScript/server.js
+++ JavaScript/server.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 const http = require('http');
 const path = require('path');
 
+const STATIC_PATH = path.join(process.cwd(), './static');
+const API_PATH = './api/';
+
 const MIME_TYPES = {
   html: 'text/html; charset=UTF-8',
   js: 'application/javascript; charset=UTF-8',
@@ -14,19 +17,84 @@ const MIME_TYPES = {
   svg: 'image/svg+xml',
 };
 
-const STATIC_PATH = path.join(process.cwd(), './static');
-
 const serveFile = name => {
   const filePath = path.join(STATIC_PATH, name);
   if (!filePath.startsWith(STATIC_PATH)) return null;
   return fs.createReadStream(filePath);
 };
 
-http.createServer((req, res) => {
-  const { url } = req;
-  const name = url === '/' ? '/index.html' : url;
-  const fileExt = path.extname(name).substring(1);
-  res.writeHead(200, { 'Content-Type': MIME_TYPES[fileExt] });
-  const stream = serveFile(name);
-  if (stream) stream.pipe(res);
+const api = new Map();
+
+const receiveArgs = async req => new Promise(resolve => {
+  const body = [];
+  req.on('data', chunk => {
+    body.push(chunk);
+  }).on('end', async () => {
+    const data = body.join('');
+    const args = JSON.parse(data);
+    resolve(args);
+  });
+});
+
+const cacheFile = name => {
+  const filePath = API_PATH + name;
+  const key = path.basename(filePath, '.js');
+  try {
+    const libPath = require.resolve(filePath);
+    delete require.cache[libPath];
+  } catch (e) {
+    return;
+  }
+  try {
+    const method = require(filePath);
+    api.set(key, method);
+  } catch (e) {
+    api.delete(name);
+  }
+};
+
+const cacheFolder = path => {
+  fs.readdir(path, (err, files) => {
+    if (err) return;
+    files.forEach(cacheFile);
+  });
+};
+
+const watch = path => {
+  fs.watch(path, (event, file) => {
+    cacheFile(file);
+  });
+};
+
+cacheFolder(API_PATH);
+watch(API_PATH);
+
+const httpError = (res, status, message) => {
+  res.statusCode = status;
+  res.end(`"${message}"`);
+};
+
+http.createServer(async (req, res) => {
+  const url = req.url === '/' ? '/index.html' : req.url;
+  const [first, second] = url.substring(1).split('/');
+  if (first === 'api') {
+    const method = api.get(second);
+    const args = await receiveArgs(req);
+    try {
+      const result = await method(...args);
+      if (!result) {
+        httpError(res, 500, 'Server error');
+        return;
+      }
+      res.end(JSON.stringify(result));
+    } catch (err) {
+      console.dir({ err });
+      httpError(res, 500, 'Server error');
+    }
+  } else {
+    const fileExt = path.extname(url).substring(1);
+    res.writeHead(200, { 'Content-Type': MIME_TYPES[fileExt] });
+    const stream = serveFile(url);
+    if (stream) stream.pipe(res);
+  }
 }).listen(8000);
